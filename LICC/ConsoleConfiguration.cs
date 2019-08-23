4c5e938bc9acda582260a4472ea2f6a8105a9ada
Jimmy Cushnie <hello@jimmycushnie.com>
2019-08-23 18:47:28 -0400

    explicitly declare CaseSensitiveCommandNames default as false

--- LICC/ConsoleConfiguration.cs
+++ LICC/ConsoleConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public sealed class ConsoleConfiguration
     {
-        public bool CaseSensitiveCommandNames { get; set; }
+        public bool CaseSensitiveCommandNames { get; set; } = false;
         public bool RunAutoExecAtStartup { get; set; } = true;
     }
 }
