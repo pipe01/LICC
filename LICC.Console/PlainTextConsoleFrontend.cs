6a1fdb04a9c5a46a33ddc29d4afc467924364abc
Felipe Martinez <pipernene@gmail.com>
2019-08-23 19:32:20 +0200

    Added plain text console frontend

---
+++ LICC.Console/PlainTextConsoleFrontend.cs
@@ -0,0 +1,42 @@
+﻿using LICC.API;
+using System;
+using SConsole = System.Console;
+
+namespace LICC.Console
+{
+    public class PlainTextConsoleFrontend : Frontend
+    {
+        public void BeginRead()
+        {
+            while (true)
+            {
+                SConsole.Write("> ");
+                string line = SConsole.ReadLine();
+                OnLineInput(line);
+            }
+        }
+
+        public override void Write(string str, Color color)
+        {
+            var prev = SConsole.ForegroundColor;
+            SConsole.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), color.ToString());
+            SConsole.Write(str);
+            SConsole.ForegroundColor = prev;
+        }
+
+        public static void StartDefault(string fileSystemRoot = null)
+        {
+            var frontend = new PlainTextConsoleFrontend();
+            var console = fileSystemRoot == null ? new CommandConsole(frontend) : new CommandConsole(frontend, fileSystemRoot);
+
+            foreach (var item in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                console.Commands.RegisterCommandsIn(item);
+            }
+
+            console.RunAutoexec();
+
+            frontend.BeginRead();
+        }
+    }
+}
