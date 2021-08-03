e66c74286abfe842d40585e014a2ae37b56a680b
Jimmy Cushnie <jimmy@mousehatgames.com>
2021-08-03 01:30:05 -0400

    Added SetWindowTitle built-in command to console frontend

--- LICC.Console/BuiltInCommands.cs
+++ LICC.Console/BuiltInCommands.cs
@@ -4,10 +4,16 @@ namespace LICC.Console
 {
     internal static class BuiltInCommands
     {
-        [Command]
-        private static void Exit()
+        [Command(Description = "Exit the program.")]
+        static void Exit()
         {
             Environment.Exit(0);
+        }
+
+        [Command(Description = "Set the title of the window.")]
+        static void SetWindowTitle(string newTitle)
+        {
+            System.Console.Title = newTitle;
         }
     }
 }
